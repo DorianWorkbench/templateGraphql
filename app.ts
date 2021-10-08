b88ab8e89e4bd3d46df0dc58f209e9cde017b3d9
Dorian <prhdorian@gmail.com>
2021-10-08 22:23:14 +0200

    remove unused import

--- app.ts
+++ app.ts
@@ -1,10 +1,7 @@
 import express, {Express, NextFunction, Request, Response} from "express";
 import { graphqlHTTP } from "express-graphql";
 import { CustomError } from "./src/errors/CustomError";
-import { ServiceError } from "./src/errors/ServiceError";
-import { ValidationError } from "./src/errors/ValidationError";
 import {GraphqlClient} from "./src/graphql/index";
-import { UserType } from "./src/graphql/types/User";
 import { UserService } from "./src/service/UserService";
 
 const app:Express = express();
@@ -14,6 +11,7 @@ const graphqlClient:GraphqlClient = new GraphqlClient(new UserService());
 
 app.use('/graphql', graphqlHTTP(async()=>({ schema: await graphqlClient.generateGraphqlSchema() })));
 
+// TODO: Check how to 
 app.use((err:CustomError, req:Request, res:Response, next:NextFunction)=>{
     return res.status(err.statusCode).json({
         error:err.render()
